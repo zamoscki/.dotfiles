53f14e9cd54319b8f9af4550758a3143b22a280f
Aleś Niamkovič <aliaksei.niamkovich@gmail.com>
2023-10-27 16:28:20 +0200

    feat: change icon size

--- defaults/init.sh
+++ defaults/init.sh
@@ -3,7 +3,7 @@
 echo 'Set defaults'
 
 # Menu Bar
-defaults write com.apple.dock tilesize -int 64
+defaults write com.apple.dock tilesize -int 48
 defaults write com.apple.dock autohide -bool true
 
 echo 'Ok'
